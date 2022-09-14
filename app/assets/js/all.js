559de26e83bceac7298d59554fac5611c8960222
lily <a0976198586@gmail.com>
2022-09-15 06:32:13 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,5 @@
-const jsonUrl ='https://raw.githubusercontent.com/hsiangfeng/JSHomeWork/master/JSON/datastore_search.json';
-const content = document.querySelector('.content');
+const jsonUrl ='https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json';
+const content = document.getElementById('content');
 const pageid = document.getElementById('pageid');
 let jsonData = {};
 
